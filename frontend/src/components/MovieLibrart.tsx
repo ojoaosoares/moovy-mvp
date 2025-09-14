4179cdcf54e3bd3ea313166859c79b2e2acac8cb
leviathan <joaosoares@dcc.ufmg.br>
2025-09-14 11:27:29 -0300

    fix: showing library empty after fetch

--- frontend/src/components/MovieLibrart.tsx
+++ frontend/src/components/MovieLibrart.tsx
@@ -4,24 +4,29 @@ import ShowMovies from './ShowMovies';
 
 const MovieLibrary: React.FC = () => {
   const [movies, setMovies] = useState<MovieDto[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchFavorites = async () => {
+      setLoading(true);
       try {
         const response = await fetch('http://localhost:4000/favorites');
         const data = await response.json();
 
         console.log(data);
 
-        const favoritesWithFlag = (data.favorites || []).map((movie: Partial<MovieDto>) => ({
-          ...movie,
-          isFavorite: true
-        }));
+        const favoritesWithFlag = (data.favorites || []).map(
+          (movie: Partial<MovieDto>) => ({
+            ...movie,
+            isFavorite: true,
+          })
+        );
         console.log(favoritesWithFlag);
         setMovies(favoritesWithFlag);
       } catch (error) {
         console.error('Erro ao buscar favoritos:', error);
       }
+      setLoading(false);
     };
 
     fetchFavorites();
@@ -29,7 +34,7 @@ const MovieLibrary: React.FC = () => {
 
   return (
     <div style={{ width: '80%', maxWidth: '800px', margin: '0 auto' }}>
-      <h2 style={{ marginLeft: '2.2rem' }}> Search</h2>
+      <h2 style={{ marginLeft: '2.2rem' }}> Library</h2>
 
       <div
         style={{
@@ -40,9 +45,7 @@ const MovieLibrary: React.FC = () => {
         }}
       >
         {movies.length > 0 && <ShowMovies movies={movies} />}
-        {movies.length === 0 && (
-          <h3>Library Empty</h3>
-        )}
+        {movies.length === 0 && !loading && <h3>Library Empty</h3>}
       </div>
     </div>
   );
