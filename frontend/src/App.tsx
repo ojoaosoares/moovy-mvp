ce0e69ca6697ea9268fb2efb69fef44102b43e5d
leviathan <joaosoares@dcc.ufmg.br>
2025-09-14 02:05:55 -0300

    feat: setting library route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MovieSearch from './components/MovieSearch';
+import MovieLibrary from './components/MovieLibrart';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <MovieSearch />
-    </div>
+      <Routes>
+        <Route path="/" element={<MovieSearch />} />
+        <Route path="/library" element={<MovieLibrary />} />
+      </Routes>
+    </Router>
   );
 }
 
