351bd6cd2823a9aaa1dc3341efe9df0e9aeb8e37
leviathan <joaosoares@dcc.ufmg.br>
2025-09-16 01:37:36 -0300

    feat: show movie component

---
+++ mobile/components/ShowMovies.tsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { ScrollView, StyleSheet, View, Dimensions } from 'react-native';
+import { MovieDto } from '../types';
+import MovieCard from './MovieCard';
+
+interface ShowMoviesProps {
+  movies: MovieDto[];
+}
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+const CARD_WIDTH = SCREEN_WIDTH * 0.7;
+const CARD_MARGIN = 16; 
+const SNAP_INTERVAL = CARD_WIDTH + CARD_MARGIN;
+
+const PADDING_HORIZONTAL = (SCREEN_WIDTH - CARD_WIDTH) / 2 - CARD_MARGIN / 2;
+
+const ShowMovies: React.FC<ShowMoviesProps> = ({ movies }) => {
+  const [displayedMovies, setDisplayedMovies] = useState<MovieDto[]>([]);
+
+  useEffect(() => {
+    let index = 0;
+    setDisplayedMovies([]);
+
+    const interval = setInterval(() => {
+      if (index < movies.length) {
+        const movie = movies[index];
+        if (movie) {
+          setDisplayedMovies((prev) => [...prev, movie]);
+        }
+        index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, [movies]);
+
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={styles.container}
+      
+      snapToInterval={SNAP_INTERVAL}
+      snapToAlignment="start"
+      decelerationRate="fast"
+      disableIntervalMomentum={true}
+    >
+      {displayedMovies.map(
+        (movie) =>
+          movie && (
+            <View key={movie.imdbID} style={styles.cardWrapper}>
+              <MovieCard movie={movie} />
+            </View>
+          )
+      )}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    paddingHorizontal: PADDING_HORIZONTAL,
+  },
+  cardWrapper: {
+    marginHorizontal: CARD_MARGIN / 2,
+  },
+});
+
+export default ShowMovies;
