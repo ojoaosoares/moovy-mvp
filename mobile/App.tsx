2dfdfe0f6519d80fd19a723d4966321137019a99
leviathan <joaosoares@dcc.ufmg.br>
2025-09-16 01:38:00 -0300

    feat: app calling library screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import LibraryScreen from './screens/LibraryScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>SEX</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <LibraryScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
