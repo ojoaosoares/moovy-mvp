b092d5fef48fad58d1d0d92f42bd48d0a5ea9cb7
leviathan <joaosoares@dcc.ufmg.br>
2025-09-21 21:34:52 -0300

    feat: library hook

--- frontend/src/screens/LibraryScreen.tsx
+++ frontend/src/screens/LibraryScreen.tsx
@@ -1,36 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { FavoriteDTO, HasFavoriteDTO } from '../types';
+import React from 'react';
 import ShowMovies from '../components/ShowMovies';
+import { useLibrary } from '../hooks/useLibrary';
 
 const LibraryScreen: React.FC = () => {
-  const [movies, setMovies] = useState<HasFavoriteDTO[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchFavorites = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch('http://localhost:4000/favorites');
-        const data: FavoriteDTO[] = await response.json();
-        const isFavMovies: HasFavoriteDTO[] = data.map(
-          (movie: FavoriteDTO) => ({
-            favorite: movie,
-            hasFavorite: true,
-          })
-        );
-        setMovies(isFavMovies);
-      } catch (error) {
-        console.error('Error fetching favorites:', error);
-      }
-      setLoading(false);
-    };
-
-    fetchFavorites();
-  }, []);
+  const { movies, loading, error } = useLibrary();
 
   return (
     <div style={{ width: '80%', maxWidth: '800px', margin: '0 auto' }}>
-      <h2 style={{ marginLeft: '2.2rem' }}> Library</h2>
+      <h2 style={{ marginLeft: '2.2rem' }}>Library</h2>
 
       <div
         style={{
@@ -40,8 +17,10 @@ const LibraryScreen: React.FC = () => {
           width: '100%',
         }}
       >
-        {movies.length > 0 && <ShowMovies movies={movies} />}
-        {movies.length === 0 && !loading && <h3>Library Empty</h3>}
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {!loading && movies.length > 0 && <ShowMovies movies={movies} />}
+        {!loading && movies.length === 0 && !error && <h3>Library Empty</h3>}
       </div>
     </div>
   );
