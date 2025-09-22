abae45c8f35beb9e9aa78ed7b95a415bfc65a083
leviathan <joaosoares@dcc.ufmg.br>
2025-09-22 09:20:08 -0300

    feat: movie mapper.

--- backend/src/modules/movie/movie.controller.ts
+++ backend/src/modules/movie/movie.controller.ts
@@ -1,24 +1,19 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { MovieService } from './movie.service';
-import { MovieDto } from './movie.dto';
+import { MovieDto } from './dto/movie.dto';
 
 @Controller('movies')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Get('search')
-  async search(
-    @Query('q') query: string,
-  ): Promise<{ movies?: MovieDto[]; error?: string }> {
+  async search(@Query('q') query: string): Promise<MovieDto[]> {
     try {
       const movies = await this.movieService.searchMovies(query);
-      if (movies.length === 0) {
-        return { error: 'No movies found' };
-      }
-      return { movies };
+      return movies;
     } catch (err: unknown) {
       if (err instanceof Error) {
-        return { error: err.message };
+        throw new Error(`Error searching for movies: ${err.message}`);
       }
       throw new Error('Error searching for movies');
     }
