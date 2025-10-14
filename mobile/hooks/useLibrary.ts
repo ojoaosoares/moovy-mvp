981718b96ddfc258625d32e7d5998ecea625cd85
João Soares <joao.santos@tarken.ag>
2025-10-14 17:57:49 -0300

    Adding todo tasks

--- mobile/hooks/useLibrary.ts
+++ mobile/hooks/useLibrary.ts
@@ -3,7 +3,8 @@ import { HasFavoriteDTO } from '../types';
 import { LibraryService } from '../modules/library/library.service';
 
 const libraryService = new LibraryService();
-
+// TODO reiniciar o component quando puxar para baixo, 
+// e adicionar mounted pra evitar que o estado seja atualizado depois do componente desmonstado
 export function useLibrary() {
   const [movies, setMovies] = useState<HasFavoriteDTO[]>([]);
   const [loading, setLoading] = useState(true);
