fb096d7f78e8ff79ab940f583df7d7f1d9228502
ojoaosoares <ojoaosoaressantos@gmail.com>
2026-04-12 19:31:21 -0300

    feat: implementing redis cache

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,16 @@ import { FavoriteModule } from './modules/favorite/favorite.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
-
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet'
 @Module({
   imports: [
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
